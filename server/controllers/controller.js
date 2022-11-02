d03d84eac86ecf2e003c86a0ecbb28dae8e26de7
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-02 14:49:39 -0600

    fixed the path

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
     landingPage: (req, res) => {
-        res.sendFile(path.join(__dirname, "../../index.html"))
+        res.sendFile(path.join(__dirname, "../../client/index.html"))
     }
 }
